283915b13624f4d450c681efaa2890b51bb77d40
vincUk <vincuk@tut.by>
2017-03-07 13:07:56 +0100

    Add print and string examples

---
+++ listing02.py
@@ -0,0 +1,8 @@
+# Print the grid using variables
+
+verticalLines = "   |   |"
+horizontalLine = "-----------\n"
+
+print( verticalLines + "\n" + horizontalLine + \
+       verticalLines + "\n" + horizontalLine + \
+       verticalLines )
