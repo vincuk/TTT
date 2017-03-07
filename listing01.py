00d5ac889380b657c37b91c6503abefb033deaff
vincUk <vincuk@tut.by>
2017-03-07 16:24:00 +0100

    Delete moved files

--- listing01.py
+++
@@ -1,8 +0,0 @@
-# First example: just print the grid!
-# But we can see in this example couples of repeating lines.
-
-print( "   |   |" )
-print( "-----------" )
-print( "   |   |" )
-print( "-----------" )
-print( "   |   |" )
